5dc213475e6811216144657d15044d6b16d4d7bd
omaremam32 <omaromohamed2003@gmail.com>
2025-11-02 10:40:26 +0200

    added hello

---
+++ print('hello').py
@@ -0,0 +1,10 @@
+name="emam"
+age=20
+N=input('enter your name: ')
+A=int(input('enter your age: '))
+if N==name:
+    print(name)
+if A>=age:
+    print('you are +20 you can pass')
+else:
+    print('you are under 20 you cannot pass')
